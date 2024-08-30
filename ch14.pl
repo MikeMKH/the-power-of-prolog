0f91cc2209af292c5e66c1629cc5a6244ddd79b7
Mike Harris <MikeMKH@users.noreply.github.com>
2024-08-30 05:50:41 -0500

    add ...

--- ch14.pl
+++ ch14.pl
@@ -35,3 +35,25 @@ seqq([Es|Ess]) -->
 
 % ?- phrase(seqq(["ab","cd","ef"]), Ls).
 % Ls = [a, b, c, d, e, f].
+
+... --> [] | [_], ... .
+
+% ?- phrase((...,[Last]), "Lily").
+% Last = y ;
+% false.
+
+% ?- phrase((...,[E,E],...), Ls).
+% Ls = [E, E] ;
+% Ls = [E, E, _] ;
+% Ls = [E, E, _, _] ;
+% Ls = [E, E, _, _, _] ;
+% Ls = [E, E, _, _, _, _] .
+
+% ?- length(Ls, _), phrase((...,[E,E],...), Ls).
+% Ls = [E, E] ;
+% Ls = [E, E, _] ;
+% Ls = [_, E, E] ;
+% Ls = [E, E, _, _] ;
+% Ls = [_, E, E, _] ;
+% Ls = [_, _, E, E] ;
+% Ls = [E, E, _, _, _] .
