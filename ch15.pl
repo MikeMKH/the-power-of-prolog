a7eeb1aa7b18ebc9909b70fc2fb57d47121a23ce
Mike Harris <MikeMKH@users.noreply.github.com>
2024-09-06 06:09:52 -0500

    add quicksort

--- ch15.pl
+++ ch15.pl
@@ -16,3 +16,25 @@ list_pair(Ls, L-Ls) :-
 % Pairs0 = [4-[a, b, c, d], 3-[a, b, c], 5-[a, b, c, d, e], 1-[a], 2-[a, b]],
 % Pairs = [1-[a], 2-[a, b], 3-[a, b, c], 4-[a, b, c, d], 5-[a, b, c|...]].
 
+quicksort([]) --> [].
+quicksort([L|Ls]) -->
+  { partition_(Ls, L, Smaller, Bigger) },
+  quicksort(Smaller),
+  [L],
+  quicksort(Bigger).
+
+partition_([], _, [], []).
+partition_([L|Ls], Pivot, Smaller0, Bigger0) :-
+  ( L @< Pivot ->
+    Smaller0 = [L|Smaller],
+    partition_(Ls, Pivot, Smaller, Bigger0)
+  ; Bigger0 = [L|Bigger],
+    partition_(Ls, Pivot, Smaller0, Bigger)
+  ).
+
+% ?- lists(Lists), phrase(quicksort(Lists), Sorted).
+% Lists = [[a, b, c, d], [a, b, c], [a, b, c, d, e], [a], [a, b]],
+% Sorted = [[a], [a, b], [a, b, c], [a, b, c, d], [a, b, c, d|...]].
+
+% ?- phrase(quicksort([a,b,c,1,5,0,x]), Ls).
+% Ls = [0, 1, 5, a, b, c, x].
