76f154e865f21b226b2d677252c689fd1b55fac8
Mike Harris <MikeMKH@users.noreply.github.com>
2024-09-19 05:48:18 -0500

    add natnum

---
+++ ch18.pl
@@ -0,0 +1,39 @@
+:- use_module(library(clpfd)).
+
+natnum(0).
+natnum(s(X)) :-
+  natnum(X).
+
+% ?- Goal = natnum(X), Goal.
+% Goal = natnum(0),
+% X = 0 ;
+% Goal = natnum(s(0)),
+% X = s(0) ;
+% Goal = natnum(s(s(0))),
+% X = s(s(0)) ;
+% Goal = natnum(s(s(s(0)))),
+% X = s(s(s(0))) .
+
+% ?- trace.
+% true.
+
+% [trace]  ?- Goal = natnum(X), Goal.
+%    Call: (13) _33942=natnum(_33938) ? creep
+%    Exit: (13) natnum(_33938)=natnum(_33938) ? creep
+%    Call: (13) natnum(_33938) ? creep
+%    Exit: (13) natnum(0) ? creep
+% Goal = natnum(0),
+% X = 0 ;
+%    Redo: (13) natnum(_33938) ? creep
+%    Call: (14) natnum(_40606) ? creep
+%    Exit: (14) natnum(0) ? creep
+%    Exit: (13) natnum(s(0)) ? creep
+% Goal = natnum(s(0)),
+% X = s(0) ;
+%    Redo: (14) natnum(_40606) ? creep
+%    Call: (15) natnum(_45792) ? creep
+%    Exit: (15) natnum(0) ? creep
+%    Exit: (14) natnum(s(0)) ? creep
+%    Exit: (13) natnum(s(s(0))) ? creep
+% Goal = natnum(s(s(0))),
+% X = s(s(0)) .
