cc8b081339d5e3e476be27832b8d65ed2d76834f
Mike Harris <MikeMKH@users.noreply.github.com>
2024-09-20 05:39:16 -0500

    add mi1

--- ch18.pl
+++ ch18.pl
@@ -52,3 +52,49 @@ complicated_clause(A) :-
 % ?- clause(complicated_clause(Z), Body).
 % Body = (goal1(Z), goal2(Z), goal3(Z)).
 
+mi1(true).
+mi1((A,B)) :-
+  mi1(A),
+  mi1(B).
+mi1(Goal) :-
+  Goal \= true,
+  Goal \= (_,_),
+  clause(Goal, Body),
+  mi1(Body).
+
+% ?- mi1(natnum(X)).
+% X = 0 ;
+% X = s(0) ;
+% X = s(s(0)) ;
+% X = s(s(s(0))) .
+
+% [trace]  ?- mi1(natnum(X)).
+%    Call: (12) mi1(natnum(_88244)) ? creep
+%    Call: (13) natnum(_88244)\=true ? creep
+%    Exit: (13) natnum(_88244)\=true ? creep
+%    Call: (13) natnum(_88244)\=(_91196, _91198) ? creep
+%    Exit: (13) natnum(_88244)\=(_91196, _91198) ? creep
+% ^  Call: (13) clause(natnum(_88244), _92820) ? creep
+% ^  Exit: (13) clause(natnum(0), true) ? creep
+%    Call: (13) mi1(true) ? creep
+%    Exit: (13) mi1(true) ? creep
+%    Exit: (12) mi1(natnum(0)) ? creep
+% X = 0 ;
+%    Redo: (13) mi1(true) ? creep
+%    Call: (14) true\=true ? creep
+%    Fail: (14) true\=true ? creep
+%    Fail: (13) mi1(true) ? creep
+% ^  Redo: (13) clause(natnum(_88244), _92820) ? creep
+% ^  Exit: (13) clause(natnum(s(_102520)), natnum(_102520)) ? creep
+%    Call: (13) mi1(natnum(_102520)) ? creep
+%    Call: (14) natnum(_102520)\=true ? creep
+%    Exit: (14) natnum(_102520)\=true ? creep
+%    Call: (14) natnum(_102520)\=(_105776, _105778) ? creep
+%    Exit: (14) natnum(_102520)\=(_105776, _105778) ? creep
+% ^  Call: (14) clause(natnum(_102520), _107400) ? creep
+% ^  Exit: (14) clause(natnum(0), true) ? creep
+%    Call: (14) mi1(true) ? creep
+%    Exit: (14) mi1(true) ? creep
+%    Exit: (13) mi1(natnum(0)) ? creep
+%    Exit: (12) mi1(natnum(s(0))) ? creep
+% X = s(0) .
