a5bd1d4587102854233c7792abae27fe0fc18b88
Mike Harris <MikeMKH@users.noreply.github.com>
2024-09-25 05:43:33 -0500

    add mi_limit

--- ch18.pl
+++ ch18.pl
@@ -310,3 +310,25 @@ mi_tree(g(G), TBody => G) :-
 % X = s(s(0)),
 % T = (((true=>natnum(0))=>natnum(s(0)))=>natnum(s(s(0)))) .
 
+mi_limit(Goal, Max) :-
+  mi_limit(Goal, Max, _).
+
+mi_limit(true, N, N).
+mi_limit((A,B), N0, N) :-
+  mi_limit(A, N0, N1),
+  mi_limit(B, N1, N).
+mi_limit(g(G), N0, N) :-
+  N0 #> 0,
+  N1 #= N0 - 1,
+  mi_clause(G, Body),
+  mi_limit(Body, N1, N).
+
+% ?- mi_limit(g(natnum(X)), 3).
+% X = 0 ;
+% X = s(0) ;
+% X = s(s(0)) ;
+% false.
+
+% ?- mi_limit(g(natnum(X)), 1).
+% X = 0 ;
+% false.
