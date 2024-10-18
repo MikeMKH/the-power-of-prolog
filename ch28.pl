e5f0d47800cc4ca5fac50a57096d80238ac6bc8a
Mike Harris <MikeMKH@users.noreply.github.com>
2024-10-18 05:54:52 -0500

    add fibonacci

--- ch28.pl
+++ ch28.pl
@@ -1,4 +1,5 @@
 % :- use_module(library(tabling)). % Warning:    :- table/1 is built-in.  library(tabling) is deprecated.
+:- use_module(library(clpfd)).
 
 :- table adjacent/2.
 adjacent(a, b).
@@ -7,3 +8,17 @@ adjacent(X, Y) :- adjacent(Y, X).
 
 % ?- adjacent(X, Y), false.
 % false.
+
+:- table fibonacci/2.
+fibonacci(0, 1).
+fibonacci(1, 1).
+fibonacci(N, F) :-
+  N #> 1,
+  N1 #= N-1,
+  N2 #= N-2,
+  fibonacci(N1, F1),
+  fibonacci(N2, F2),
+  F #= F1 + F2.
+
+% ?- fibonacci(100,F).
+% F = 573147844013817084101.
